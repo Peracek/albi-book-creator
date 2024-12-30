16f8410949e54cf1c9371f195418ca4b20d049d9
Peracek <pavel.peroutka@gmail.com>
2024-12-30 15:51:27 +0100

    Initial commit

---
+++ apps/albi-book-creator/src/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="albi-book-creator" />
+    </div>
+  );
+}
+
+export default App;
