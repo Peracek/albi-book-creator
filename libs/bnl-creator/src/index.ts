c2093afcca2b93bb26f36e6141ed5b40855e5610
Peracek <pavel.peroutka@gmail.com>
2025-01-01 11:41:21 +0100

    naive bnl yaml typings

--- libs/bnl-creator/src/index.ts
+++ libs/bnl-creator/src/index.ts
@@ -1,6 +1,7 @@
 import { program } from '@commander-js/extra-typings';
 import * as fs from 'fs-extra';
 import * as yaml from 'js-yaml';
+import { BnlYamlFile } from './typings';
 
 program
   .command('load <path>')
@@ -8,9 +9,10 @@ program
   .action(async (path) => {
     try {
       const fileContents = await fs.readFile(path, 'utf8');
-      const yamlData = yaml.loadAll(fileContents);
+      const yamlData = yaml.loadAll(fileContents) as BnlYamlFile;
       const [header, quiz, oids] = yamlData;
-      console.log({ header, quiz, oids });
+      // console.log({ header, quiz, oids });
+      console.log(oids);
     } catch (error) {
       // @ts-expect-error
       console.error(`Error loading YAML file: ${error.message}`);
---
+++ libs/bnl-creator/src/typings.ts
@@ -0,0 +1,64 @@
+// HEADER
+
+export interface BnlHeader {
+  book_id: number;
+  sys_icons?: string[] | null;
+  encryption: Encryption;
+  book_mode_read: BookModeRead;
+  start_button_1st_read: StartButton1stReadOrStartButton2ndRead;
+  start_button_2nd_read: StartButton1stReadOrStartButton2ndRead;
+  unk_tbl_ptr_16?: string[] | null;
+  unk_tbl_ptr_27?: string[] | null;
+  unk_tbl_ptr_28?: string[] | null;
+  unk_tbl_ptr_29?: string[] | null;
+}
+export interface Encryption {
+  header_key: number;
+  prekey?: number[] | null;
+  prekey_dw: number;
+}
+export interface BookModeRead {
+  mode_0?: string[] | null;
+  mode_2?: string[] | null;
+}
+export interface StartButton1stReadOrStartButton2ndRead {
+  mode_0?: string[] | null;
+  mode_1?: string[] | null;
+  mode_2?: string[] | null;
+}
+
+// QUIZ
+
+export interface Quiz {
+  quiz_neg1?: string[] | null;
+  quiz_neg2?: string[] | null;
+  quiz_pos1?: string[] | null;
+  quiz_pos2?: string[] | null;
+  quiz_results?: string[] | null;
+  quizes?: QuizesEntity[] | null;
+}
+export interface QuizesEntity {
+  q_asked: number;
+  q_oid: string;
+  q_type: number;
+  q_unk: number;
+  questions?: QuestionsEntity[] | null;
+}
+export interface QuestionsEntity {
+  q1_good_reply_oids?: string[] | null;
+  q1_oid: string;
+  q1_unk: number;
+}
+
+// OIDS
+
+export type Oids = Record<string, Oid>;
+type Oid = {
+  mode_0?: string[] | null;
+  mode_1?: string[] | null;
+  mode_2?: string[] | null;
+};
+
+// BNL YAML FILE
+
+export type BnlYamlFile = [BnlHeader, Quiz, Oids];
