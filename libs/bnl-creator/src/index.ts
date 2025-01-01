48547429aaf7bceb73fcb72bbd750dec926f5155
Peracek <pavel.peroutka@gmail.com>
2025-01-01 11:24:36 +0100

    load yaml file

--- libs/bnl-creator/src/index.ts
+++ libs/bnl-creator/src/index.ts
@@ -1,8 +1,20 @@
 import { program } from '@commander-js/extra-typings';
+import * as fs from 'fs-extra';
+import * as yaml from 'js-yaml';
 
-program.command('list').action(async () => {
-  const things = ['one', 'two', 'three'];
-  things.forEach((thing) => console.log(thing));
-});
+program
+  .command('load <path>')
+  .description('Load a YAML file from the specified path')
+  .action(async (path) => {
+    try {
+      const fileContents = await fs.readFile(path, 'utf8');
+      const yamlData = yaml.loadAll(fileContents);
+      const [header, quiz, oids] = yamlData;
+      console.log({ header, quiz, oids });
+    } catch (error) {
+      // @ts-expect-error
+      console.error(`Error loading YAML file: ${error.message}`);
+    }
+  });
 
 program.parse();
