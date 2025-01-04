993cf8db82ce83e4b41493ab680e1d7c0e86443c
Peracek <pavel.peroutka@gmail.com>
2025-01-04 21:57:19 +0100

    working recording an upload

--- apps/albi-book-creator/src/app/AreaDetail/AreaDetailModal.tsx
+++ apps/albi-book-creator/src/app/AreaDetail/AreaDetailModal.tsx
@@ -1,7 +1,9 @@
-import { db } from '@abc/storage';
-import { Modal } from 'antd';
+import { db, ImageObject } from '@abc/storage';
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, Form, FormProps, Input, Modal, Upload } from 'antd';
 import { useLiveQuery } from 'dexie-react-hooks';
-import { AudioRecorder, UploadSound } from '../AudioRecorder';
+import { useState } from 'react';
+import { AudioRecorder } from '../AudioRecorder';
 
 type Props = {
   areaId: number;
@@ -10,20 +12,65 @@ type Props = {
 
 export const AreaDetailModal = ({ areaId, onClose }: Props) => {
   const area = useLiveQuery(() => db.imageObjects.get(areaId));
+  const [recording, setRecording] = useState<File | null>(null);
   if (!area) return null;
+
+  const onFinish: FormProps<ImageObject>['onFinish'] = (values) => {
+    db.imageObjects.update(areaId, {
+      name: values.name,
+      oid: values.oid,
+      sound: recording ?? values.sound,
+    });
+  };
 
   return (
     <Modal
-      title="Modal 1000px width"
+      title={area.name ?? 'Area'}
       centered
       open
       onOk={onClose}
       onCancel={onClose}
     >
-      <p>{area.name}</p>
-      <p>{area.oid}</p>
-      <AudioRecorder imageObject={area} />
-      <UploadSound imageObject={area} />
+      <Form
+        name="basic"
+        layout="vertical"
+        initialValues={area}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item<ImageObject> label="Name" name="name">
+          <Input />
+        </Form.Item>
+        <Form.Item<ImageObject> label="OID" name="oid">
+          <Input />
+        </Form.Item>
+        <Form.Item<ImageObject> label="Sound" name="sound">
+          <Upload accept=".mp3" beforeUpload={() => false}>
+            <AudioRecorder onRecorded={setRecording} />
+            <Button icon={<UploadOutlined />}>Upload</Button>
+          </Upload>
+        </Form.Item>
+        <div>
+          recording
+          {recording && (
+            <audio controls src={URL.createObjectURL(recording)}></audio>
+          )}
+        </div>
+        <div>
+          original sound
+          {area.sound && (
+            <audio controls>
+              <source
+                src={URL.createObjectURL(area.sound)}
+                type="audio/mp3"
+              ></source>
+            </audio>
+          )}
+        </div>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form>
     </Modal>
   );
 };
